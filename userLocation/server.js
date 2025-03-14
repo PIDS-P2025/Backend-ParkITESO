962a4d3c7c47462280445b89fe032f8a1b7119be
Octavio Valdez <90404916+OctavioValdez@users.noreply.github.com>
2025-03-13 18:44:43 -0600

    User location backend

---
+++ userLocation/server.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const SERVER_IP = process.env.SERVER_IP;
+
+app.post("/location", async (req, res) => {
+    const { latitude, longitude } = req.body;
+
+    if (!latitude || !longitude) {
+        return res.status(400).json({ error: "No coordinates" });
+    }
+
+    try {
+        const response = await axios.post(`${SERVER_IP}/process-location`, {
+            latitude,
+            longitude,
+        });
+
+        res.json({
+            message: "Coordinates sent",
+            ec2_response: response.data,
+        });
+    } catch (error) {
+        console.error("Error sending", error);
+        res.status(500).json({ error: "Error with server" });
+    }
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
